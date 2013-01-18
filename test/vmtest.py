df55ccf11385382f7831608faedd2442682903e3
Ned Batchelder <ned@nedbatchelder.com>
2013-01-18 07:46:08 -0500

    Improved diagnostics: stdout is displayed at the end of the test, assertion failures are raised, disassemble all of the code not just the top level.

--- test/vmtest.py
+++ test/vmtest.py
@@ -3,11 +3,21 @@
 import dis
 import sys
 import textwrap
+import types
 import unittest
 from cStringIO import StringIO
 
 from byterun.pyvm2 import VirtualMachine, VirtualMachineError
 
+def dis_code(code):
+    """Disassemble `code` and all the code it refers to."""
+    for const in code.co_consts:
+        if isinstance(const, types.CodeType):
+            dis_code(const)
+
+    print
+    print code
+    dis.dis(code)
 
 class VmTestCase(unittest.TestCase):
 
@@ -18,10 +28,11 @@ class VmTestCase(unittest.TestCase):
         code = compile(code, "<string>", "exec")
 
         # Print the disassembly so we'll see it if the test fails.
-        print
-        dis.dis(code)
+        dis_code(code)
 
-        old_stdout = sys.stdout
+        real_stdout = sys.stdout
+
+        # Run the code through our VM.
 
         vm_stdout = StringIO()
         sys.stdout = vm_stdout
@@ -31,12 +42,24 @@ class VmTestCase(unittest.TestCase):
         vm_value = vm_exc = None
         try:
             vm_value = vm.run()
-        except VirtualMachineError:
+        except VirtualMachineError:         # pragma: no cover
+            # If the VM code raises an error, show it.
+            raise
+        except AssertionError:              # pragma: no cover
+            # If test code fails an assert, show it.
             raise
         except Exception, vm_exc:
+            # Otherwise, keep the exception for comparison later.
             pass
         finally:
-            old_stdout.write("\n".join(vm._log))
+            if vm._log:                     # pragma: no branch
+                real_stdout.write("-- VM log ----------\n")
+                real_stdout.write("\n".join(vm._log))
+                real_stdout.write("\n")
+            real_stdout.write("-- stdout ----------\n")
+            real_stdout.write(vm_stdout.getvalue())
+
+        # Run the code through the real Python interpreter, for comparison.
 
         py_stdout = StringIO()
         sys.stdout = py_stdout
@@ -44,10 +67,12 @@ class VmTestCase(unittest.TestCase):
         py_value = py_exc = None
         try:
             py_value = eval(code)
+        except AssertionError:              # pragma: no cover
+            raise
         except Exception, py_exc:
             pass
 
-        sys.stdout = old_stdout
+        sys.stdout = real_stdout
 
         self.assert_same_exception(vm_exc, py_exc)
         self.assertEqual(vm_stdout.getvalue(), py_stdout.getvalue())
