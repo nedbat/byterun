adfd0cc95b224d8a73fd74b92510d8cb042be431
Ned Batchelder <ned@nedbatchelder.com>
2013-02-01 21:54:30 -0500

    Move some tests around.

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,5 +1,8 @@
+"""Basic tests for Byterun."""
+
 from __future__ import print_function
 from . import vmtest
+
 
 class TestIt(vmtest.VmTestCase):
     def test_constant(self):
@@ -15,16 +18,6 @@ class TestIt(vmtest.VmTestCase):
                 print("hello")
             fn()
             print("bye")
-            """)
-
-    def test_recursive_functions(self):
-        self.assert_ok("""\
-            def pow(b, e):
-                if e == 0:
-                    return 1
-                else:
-                    return b*pow(b, e-1)
-            print("2**6 is %d" % pow(2, 6))
             """)
 
     def test_for_loop(self):
@@ -152,40 +145,6 @@ class TestIt(vmtest.VmTestCase):
             print(-x, ~x, not x)
             """)
 
-    def test_functions(self):
-        self.assert_ok("""\
-            def fn(a, b=17, c="Hello", d=[]):
-                d.append(99)
-                print(a, b, c, d)
-            fn(1)
-            fn(2, 3)
-            fn(3, c="Bye")
-            fn(4, d=["What?"])
-            fn(5, "b", "c")
-            """)
-
-    def test_recursion(self):
-        self.assert_ok("""\
-            def fact(n):
-                if n <= 1:
-                    return 1
-                else:
-                    return n * fact(n-1)
-            f6 = fact(6)
-            print(f6)
-            assert f6 == 720
-            """)
-
-    def test_args_kwargs_functions(self):
-        self.assert_ok("""\
-            def fn(a, b=17, c="Hello", d=[]):
-                d.append(99)
-                print(a, b, c, d)
-            fn(6, *[77, 88])
-            fn(**{'c': 23, 'a': 7})
-            fn(6, *[77], **{'c': 23, 'd': [123]})
-            """)
-
     def test_attributes(self):
         self.assert_ok("""\
             l = lambda: 1   # Just to have an object...
@@ -247,83 +206,6 @@ class TestIt(vmtest.VmTestCase):
             assert l == ["ABC", "xyz"]
             """)
 
-class TestClosures(vmtest.VmTestCase):
-    def test_closures(self):
-        self.assert_ok("""\
-            def make_adder(x):
-                def add(y):
-                    return x+y
-                return add
-            a = make_adder(10)
-            print(a(7))
-            assert a(7) == 17
-            """)
-
-    def test_closures_store_deref(self):
-        self.assert_ok("""\
-            def make_adder(x):
-                z = x+1
-                def add(y):
-                    return x+y+z
-                return add
-            a = make_adder(10)
-            print(a(7))
-            assert a(7) == 28
-            """)
-
-    def test_closures_in_loop(self):
-        self.assert_ok("""\
-            def make_fns(x):
-                fns = []
-                for i in range(x):
-                    fns.append(lambda: i)
-                return fns
-            fns = make_fns(3)
-            for f in fns:
-                print(f())
-            assert fns[0]() == fns[1]() == fns[2]() == 2
-            """)
-
-    def test_closures_with_defaults(self):
-        self.assert_ok("""\
-            def make_adder(x, y=13, z=43):
-                def add(q, r=11):
-                    return x+y+z+q+r
-                return add
-            a = make_adder(10, 17)
-            print(a(7))
-            assert a(7) == 88
-            """)
-
-    def test_deep_closures(self):
-        self.assert_ok("""\
-            def f1(a):
-                b = 2*a
-                def f2(c):
-                    d = 2*c
-                    def f3(e):
-                        f = 2*e
-                        def f4(g):
-                            h = 2*g
-                            return a+b+c+d+e+f+g+h
-                        return f4
-                    return f3
-                return f2
-            answer = f1(3)(4)(5)(6)
-            print(answer)
-            assert answer == 54
-            """)
-
-class TestGenerators(vmtest.VmTestCase):
-    def test_first(self):
-        self.assert_ok("""\
-            def two():
-                yield 1
-                yield 2
-            for i in two():
-                print(i)
-            """)
-
 
 class TestLoops(vmtest.VmTestCase):
     def test_for(self):
@@ -375,137 +257,3 @@ class TestLoops(vmtest.VmTestCase):
                     print(".")
             print("done")
             """)
-
-
-class TestExceptions(vmtest.VmTestCase):
-    def test_catching_exceptions(self):
-        # Catch the exception precisely
-        self.assert_ok("""\
-            try:
-                [][1]
-                print("Shouldn't be here...")
-            except IndexError:
-                print("caught it!")
-            """)
-        # Catch the exception by a parent class
-        self.assert_ok("""\
-            try:
-                [][1]
-                print("Shouldn't be here...")
-            except Exception:
-                print("caught it!")
-            """)
-        # Catch all exceptions
-        self.assert_ok("""\
-            try:
-                [][1]
-                print("Shouldn't be here...")
-            except:
-                print("caught it!")
-            """)
-
-    def test_raise_exception(self):
-        self.assert_ok("raise Exception('oops')", raises=Exception)
-
-    def test_raise_exception_class(self):
-        self.assert_ok("raise ValueError", raises=ValueError)
-
-    def test_raise_and_catch_exception(self):
-        self.assert_ok("""\
-            try:
-                raise ValueError("oops")
-            except ValueError as e:
-                print("Caught: %s" % e)
-            print("All done")
-            """)
-
-    def test_raise_and_catch_exception_in_function(self):
-        self.assert_ok("""\
-            def fn():
-                raise ValueError("oops")
-
-            try:
-                fn()
-            except ValueError as e:
-                print("Caught: %s" % e)
-            print("done")
-            """)
-
-    def test_global_name_error(self):
-        self.assert_ok("fooey", raises=NameError)
-        self.assert_ok("""\
-            try:
-                fooey
-                print("Yes fooey?")
-            except NameError:
-                print("No fooey")
-            """)
-
-    def test_local_name_error(self):
-        self.assert_ok("""\
-            def fn():
-                fooey
-            fn()
-            """, raises=NameError)
-
-    def test_catch_local_name_error(self):
-        self.assert_ok("""\
-            def fn():
-                try:
-                    fooey
-                    print("Yes fooey?")
-                except NameError:
-                    print("No fooey")
-            fn()
-            """)
-
-    def test_reraise(self):
-        self.assert_ok("""\
-            def fn():
-                try:
-                    fooey
-                    print("Yes fooey?")
-                except NameError:
-                    print("No fooey")
-                    raise
-            fn()
-            """, raises=NameError)
-
-    def test_reraise_explicit_exception(self):
-        self.assert_ok("""\
-            def fn():
-                try:
-                    raise ValueError("ouch")
-                except ValueError as e:
-                    print("Caught %s" % e)
-                    raise
-            fn()
-            """, raises=ValueError)
-
-    def test_finally_while_throwing(self):
-        self.assert_ok("""\
-            def fn():
-                try:
-                    print("About to..")
-                    raise ValueError("ouch")
-                finally:
-                    print("Finally")
-            fn()
-            print("Done")
-            """, raises=ValueError)
-
-    def test_coverage_issue_92(self):
-        self.assert_ok("""\
-            l = []
-            for i in range(3):
-                try:
-                    l.append(i)
-                finally:
-                    l.append('f')
-                l.append('e')
-            l.append('r')
-            print(l)
-            assert l == [0, 'f', 'e', 1, 'f', 'e', 2, 'f', 'e', 'r']
-            """)
-
-
---
+++ test/test_functions.py
@@ -0,0 +1,121 @@
+"""Test functions etc, for Byterun."""
+
+from __future__ import print_function
+from . import vmtest
+
+
+class TestFunctions(vmtest.VmTestCase):
+    def test_functions(self):
+        self.assert_ok("""\
+            def fn(a, b=17, c="Hello", d=[]):
+                d.append(99)
+                print(a, b, c, d)
+            fn(1)
+            fn(2, 3)
+            fn(3, c="Bye")
+            fn(4, d=["What?"])
+            fn(5, "b", "c")
+            """)
+
+    def test_recursion(self):
+        self.assert_ok("""\
+            def fact(n):
+                if n <= 1:
+                    return 1
+                else:
+                    return n * fact(n-1)
+            f6 = fact(6)
+            print(f6)
+            assert f6 == 720
+            """)
+
+    def test_args_kwargs_functions(self):
+        self.assert_ok("""\
+            def fn(a, b=17, c="Hello", d=[]):
+                d.append(99)
+                print(a, b, c, d)
+            fn(6, *[77, 88])
+            fn(**{'c': 23, 'a': 7})
+            fn(6, *[77], **{'c': 23, 'd': [123]})
+            """)
+
+
+class TestClosures(vmtest.VmTestCase):
+    def test_closures(self):
+        self.assert_ok("""\
+            def make_adder(x):
+                def add(y):
+                    return x+y
+                return add
+            a = make_adder(10)
+            print(a(7))
+            assert a(7) == 17
+            """)
+
+    def test_closures_store_deref(self):
+        self.assert_ok("""\
+            def make_adder(x):
+                z = x+1
+                def add(y):
+                    return x+y+z
+                return add
+            a = make_adder(10)
+            print(a(7))
+            assert a(7) == 28
+            """)
+
+    def test_closures_in_loop(self):
+        self.assert_ok("""\
+            def make_fns(x):
+                fns = []
+                for i in range(x):
+                    fns.append(lambda: i)
+                return fns
+            fns = make_fns(3)
+            for f in fns:
+                print(f())
+            assert fns[0]() == fns[1]() == fns[2]() == 2
+            """)
+
+    def test_closures_with_defaults(self):
+        self.assert_ok("""\
+            def make_adder(x, y=13, z=43):
+                def add(q, r=11):
+                    return x+y+z+q+r
+                return add
+            a = make_adder(10, 17)
+            print(a(7))
+            assert a(7) == 88
+            """)
+
+    def test_deep_closures(self):
+        self.assert_ok("""\
+            def f1(a):
+                b = 2*a
+                def f2(c):
+                    d = 2*c
+                    def f3(e):
+                        f = 2*e
+                        def f4(g):
+                            h = 2*g
+                            return a+b+c+d+e+f+g+h
+                        return f4
+                    return f3
+                return f2
+            answer = f1(3)(4)(5)(6)
+            print(answer)
+            assert answer == 54
+            """)
+
+class TestGenerators(vmtest.VmTestCase):
+    def test_first(self):
+        self.assert_ok("""\
+            def two():
+                yield 1
+                yield 2
+            for i in two():
+                print(i)
+            """)
+
+
+
