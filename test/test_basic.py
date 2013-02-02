8372372a8addcda38eaad0503c539bad56969433
Ned Batchelder <ned@nedbatchelder.com>
2013-02-02 08:54:25 -0500

    Cover the conditional bytecodes

--- test/test_basic.py
+++ test/test_basic.py
@@ -260,6 +260,36 @@ class TestIt(vmtest.VmTestCase):
             assert g['a'] == 11
             """)
 
+    def test_jump_if_true_or_pop(self):
+        self.assert_ok("""\
+            def f(a, b):
+                return a or b
+            assert f(17, 0) == 17
+            assert f(0, 23) == 23
+            assert f(0, "") == ""
+            """)
+
+    def test_jump_if_false_or_pop(self):
+        self.assert_ok("""\
+            def f(a, b):
+                return not(a and b)
+            assert f(17, 0) is True
+            assert f(0, 23) is True
+            assert f(0, "") is True
+            assert f(17, 23) is False
+            """)
+
+    def test_pop_jump_if_true(self):
+        self.assert_ok("""\
+            def f(a):
+                if not a:
+                    return 'foo'
+                else:
+                    return 'bar'
+            assert f(0) == 'foo'
+            assert f(1) == 'bar'
+            """)
+
 
 class TestLoops(vmtest.VmTestCase):
     def test_for(self):
