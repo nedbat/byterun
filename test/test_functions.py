f994af681ff84bcea36862fc5b86c9a643496fd5
rocky <rocky@dusytfeet.com>
2020-05-06 22:06:48 -0400

    Move another function into a bytecode file

--- test/test_functions.py
+++ test/test_functions.py
@@ -123,48 +123,7 @@ class TestFunctions(vmtest.VmTestCase):
                 """)
 
         def test_different_globals_may_have_different_builtins(self):
-            self.assert_ok("""\
-                def replace_globals(f, new_globals):
-                    import sys
-                    if sys.version_info.major == 2:
-                        args = [
-                            f.func_code,
-                            new_globals,
-                            f.func_name,
-                            f.func_defaults,
-                            f.func_closure,
-                        ]
-                    else:
-                        args = [
-                            f.__code__,
-                            new_globals,
-                            f.__name__,
-                            f.__defaults__,
-                            f.__closure__,
-                        ]
-                    if hasattr(f, '_vm'):
-                        name = args.remove(args[2])
-                        args.insert(0, name)
-                        args.append(f._vm)
-                    return type(lambda: None)(*args)
-
-
-                def f():
-                    assert g() == 2
-                    assert a == 1
-
-
-                def g():
-                    return a  # a is in the builtins and set to 2
-
-
-                # g and f have different builtins that both provide ``a``.
-                g = replace_globals(g, {'__builtins__': {'a': 2}})
-                f = replace_globals(f, {'__builtins__': {'a': 1}, 'g': g})
-
-
-                f()
-                """)
+            self.do_one()
 
         def test_no_builtins(self):
             self.do_one()
