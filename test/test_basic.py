3a8bb2c98e7efa2eb77464cd396131151d891747
rocky <rocky@dusytfeet.com>
2021-03-30 04:14:05 -0400

    Go over 3.6 bytecode

--- test/test_basic.py
+++ test/test_basic.py
@@ -7,21 +7,25 @@ except ImportError:
     from . import vmtest
 
 from xdis import PYTHON3, PYTHON_VERSION
+
 PY2 = not PYTHON3
 
-class TestBasic(vmtest.VmTestCase):
 
+class TestBasic(vmtest.VmTestCase):
     def test_attribute_access(self):
         self.self_checking()
 
-        self.assert_ok("""\
+        self.assert_ok(
+            """\
             class Thing2(object):
                 z = 17
                 def __init__(self):
                     self.x = 23
             t = Thing2()
             print(t.xyzzy)
-            """, raises=AttributeError)
+            """,
+            raises=AttributeError,
+        )
 
     def test_bound_method_on_falsy_objects(self):
         self.self_checking()
@@ -59,13 +63,15 @@ class TestBasic(vmtest.VmTestCase):
     def test_subscripting(self):
         self.self_checking()
 
-    if PYTHON_VERSION in (3.6, 3.7, 3.8, 3.9):
+    if PYTHON_VERSION in (3.7, 3.8, 3.9):
         print("Test not gone over yet for %s" % PYTHON_VERSION)
     else:
 
         if PY2:
+
             def test_inplace_division(self):
-                self.assert_ok("""\
+                self.assert_ok(
+                    """\
                     x, y = 24, 3
                     x /= y
                     assert x == 8 and y == 3
@@ -73,10 +79,14 @@ class TestBasic(vmtest.VmTestCase):
                     x /= y
                     assert x == 2 and y == 3
                     assert isinstance(x, int)
-                    """)
+                    """
+                )
+
         elif PYTHON3:
+
             def test_inplace_division(self):
-                self.assert_ok("""\
+                self.assert_ok(
+                    """\
                     x, y = 24, 3
                     x /= y
                     assert x == 8.0 and y == 3
@@ -84,46 +94,58 @@ class TestBasic(vmtest.VmTestCase):
                     x /= y
                     assert x == (8.0/3.0) and y == 3
                     assert isinstance(x, float)
-                    """)
+                    """
+                )
 
         def test_strange_sequence_ops(self):
             # from stdlib: test/test_augassign.py
             self.do_one()
 
         def test_unary_operators(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 x = 8
                 print(-x, ~x, not x)
-                """)
+                """
+            )
 
         def test_attribute_inplace_ops(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 l = lambda: 1   # Just to have an object...
                 l.foo = 17
                 l.foo -= 3
                 print(l.foo)
-                """)
+                """
+            )
 
         def test_deleting_names(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 g = 17
                 assert g == 17
                 del g
                 g
-                """, raises=NameError)
+                """,
+                raises=NameError,
+            )
 
         def test_deleting_local_names(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 def f():
                     l = 23
                     assert l == 23
                     del l
                     l
                 f()
-                """, raises=NameError)
+                """,
+                raises=NameError,
+            )
 
         def test_classes(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Thing(object):
                     def __init__(self, x):
                         self.x = x
@@ -133,10 +155,12 @@ class TestBasic(vmtest.VmTestCase):
                 thing2 = Thing(3)
                 print(thing1.x, thing2.x)
                 print(thing1.meth(4), thing2.meth(5))
-                """)
+                """
+            )
 
         def test_calling_subclass_methods(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Thing(object):
                     def foo(self):
                         return 17
@@ -146,10 +170,12 @@ class TestBasic(vmtest.VmTestCase):
 
                 st = SubThing()
                 print(st.foo())
-                """)
+                """
+            )
 
         def test_subclass_attribute(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Thing(object):
                     def __init__(self):
                         self.foo = 17
@@ -157,10 +183,12 @@ class TestBasic(vmtest.VmTestCase):
                     pass
                 st = SubThing()
                 print(st.foo)
-                """)
+                """
+            )
 
         def test_subclass_attributes_not_shared(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Thing(object):
                     foo = 17
                 class SubThing(Thing):
@@ -169,18 +197,23 @@ class TestBasic(vmtest.VmTestCase):
                 t = Thing()
                 assert st.foo == 25
                 assert t.foo == 17
-                """)
+                """
+            )
 
         def test_object_attrs_not_shared_with_class(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Thing(object):
                     pass
                 t = Thing()
                 t.foo = 1
-                Thing.foo""", raises=AttributeError)
+                Thing.foo""",
+                raises=AttributeError,
+            )
 
         def test_data_descriptors_precede_instance_attributes(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Foo(object):
                     pass
                 f = Foo()
@@ -192,10 +225,12 @@ class TestBasic(vmtest.VmTestCase):
                         raise NotImplementedError
                 Foo.des = Descr()
                 assert f.des == 2
-                """)
+                """
+            )
 
         def test_instance_attrs_precede_non_data_descriptors(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Foo(object):
                     pass
                 f = Foo()
@@ -205,10 +240,12 @@ class TestBasic(vmtest.VmTestCase):
                         return 2
                 Foo.des = Descr()
                 assert f.des == 3
-                """)
+                """
+            )
 
         def test_subclass_attributes_dynamic(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Foo(object):
                     pass
                 class Bar(Foo):
@@ -216,10 +253,12 @@ class TestBasic(vmtest.VmTestCase):
                 b = Bar()
                 Foo.baz = 3
                 assert b.baz == 3
-                """)
+                """
+            )
 
         def test_staticmethods(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Thing(object):
                     @staticmethod
                     def smeth(x):
@@ -230,57 +269,71 @@ class TestBasic(vmtest.VmTestCase):
 
                 Thing.smeth(1492)
                 Thing.cmeth(1776)
-                """)
+                """
+            )
 
         def test_unbound_methods(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Thing(object):
                     def meth(self, x):
                         print(x)
                 m = Thing.meth
                 m(Thing(), 1815)
-                """)
+                """
+            )
 
         def test_bound_methods(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class Thing(object):
                     def meth(self, x):
                         print(x)
                 t = Thing()
                 m = t.meth
                 m(1815)
-                """)
+                """
+            )
 
         def test_unpacking(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 a, b, c = (1, 2, 3)
                 assert a == 1
                 assert b == 2
                 assert c == 3
-                """)
+                """
+            )
 
         if PY2:
+
             def test_exec_statement(self):
-                self.assert_ok("""\
+                self.assert_ok(
+                    """\
                     g = {}
                     exec "a = 11" in g, g
                     assert g['a'] == 11
-                    """)
+                    """
+                )
+
         elif PYTHON3:
+
             def test_exec_statement(self):
-                self.assert_ok("""\
+                self.assert_ok(
+                    """\
                     g = {}
                     exec("a = 11", g, g)
                     assert g['a'] == 11
-                    """)
-
+                    """
+                )
 
         def test_multiple_classes(self):
             # Making classes used to mix together all the class-scoped values
             # across classes.  This test would fail because A.__init__ would be
             # over-written with B.__init__, and A(1, 2, 3) would complain about
             # too many arguments.
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 class A(object):
                     def __init__(self, a, b, c):
                         self.sum = a + b + c
@@ -293,56 +346,67 @@ class TestBasic(vmtest.VmTestCase):
                 b = B(7)
                 print(a.sum)
                 print(b.x)
-                """)
-
+                """
+            )
 
     if PY2:
+
         class TestPrinting(vmtest.VmTestCase):
             def test_printing(self):
                 self.assert_ok("print 'hello'")
                 self.assert_ok("a = 3; print a+4")
-                self.assert_ok("""
+                self.assert_ok(
+                    """
                     print 'hi', 17, u'bye', 23,
                     print "", "\t", "the end"
-                    """)
+                    """
+                )
 
             def test_printing_in_a_function(self):
-                self.assert_ok("""\
+                self.assert_ok(
+                    """\
                     def fn():
                         print "hello"
                     fn()
                     print "bye"
-                    """)
+                    """
+                )
 
             def test_printing_to_a_file(self):
-                self.assert_ok("""\
+                self.assert_ok(
+                    """\
                     import sys
                     print >>sys.stdout, 'hello', 'there'
-                    """)
-
+                    """
+                )
 
     class TestLoops(vmtest.VmTestCase):
         def test_break(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 for i in range(10):
                     print(i)
                     if i == 7:
                         break
                 print("done")
-                """)
+                """
+            )
 
         def test_continue(self):
             # fun fact: this doesn't use CONTINUE_LOOP
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 for i in range(10):
                     if i % 3 == 0:
                         continue
                     print(i)
                 print("done")
-                """)
+                """
+            )
 
         def test_continue_in_try_except(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 for i in range(10):
                     try:
                         if i % 3 == 0:
@@ -351,10 +415,12 @@ class TestBasic(vmtest.VmTestCase):
                     except ValueError:
                         pass
                 print("done")
-                """)
+                """
+            )
 
         def test_continue_in_try_finally(self):
-            self.assert_ok("""\
+            self.assert_ok(
+                """\
                 for i in range(10):
                     try:
                         if i % 3 == 0:
@@ -363,14 +429,15 @@ class TestBasic(vmtest.VmTestCase):
                     finally:
                         print(".")
                 print("done")
-                """)
+                """
+            )
 
 
 if __name__ == "__main__":
     # import unittest
     # unittest.main()
 
-    t = TestBasic("test_decorator")
+    t = TestBasic("test_building_stuff")
     # t.test_decorator()
     # t = TestComparisons("test_in")
     # t.test_in()
