225932d5e70521d694bb20386cec29b41f6377d5
rocky <rocky@dusytfeet.com>
2021-07-08 04:56:48 -0400

    Add PyPy 3.7 to list of supported Pythons

--- xpython/version.py
+++ xpython/version.py
@@ -9,5 +9,5 @@ __version__="1.3.5.dev0"  # noqa
 
 # The below has extra commas for POSIX shell's taste, but that's okay
 SUPPORTED_PYTHON=(2.7, 3.3, 3.2, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9)  # noqa
-SUPPORTED_PYPY=(2.7, 3.2, 3.5, 3.6)  # noqa
+SUPPORTED_PYPY=(2.7, 3.2, 3.5, 3.6, 3.7)  # noqa
 SUPPORTED_BYTECODE=(2.4, 2.5, 2.6, 2.7, 3.3, 3.2, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9)  # noqa
