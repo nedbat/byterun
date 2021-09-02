d9947cb822e59a2857a0ab234ec45686ecf1947c
rocky <rocky@dusytfeet.com>
2021-09-02 05:08:07 -0400

    Fill in PyPy 3.5

--- test/add-test.sh
+++ test/add-test.sh
@@ -7,15 +7,15 @@ mydir=$(dirname ${BASH_SOURCE[0]})
 
 (cd ../../python-xdis && . ./admin-tools/setup-master.sh)
 # Note: Python < 2.7 is added at the end and 2.6.9 is used as a sentinal in the version test below
-PYENV_VERSIONS=${PYENV_VERSIONS:-"3.6.13 3.7.11 3.8.11 3.9.6 3.3.7 3.4.10 3.2.6 3.5.9 2.7.18 2.6.9 2.5.6 2.4.6"}
+PYENV_VERSIONS=${PYENV_VERSIONS:-"3.6.14 3.7.11 3.8.11 3.9.7 3.3.7 3.4.10 3.2.6 3.5.9 2.7.18 2.6.9 2.5.6 2.4.6"}
 for version in $PYENV_VERSIONS; do
     # Note: below we use
     if [[ $version == 2.6.9 ]]; then
-	(cd ../../python-xdis && . ./admin-tools/setup-python-2.4.sh)
+        cd ../../python-xdis && . ./admin-tools/setup-python-2.4.sh)
     fi
     for file in $*; do
-	pyenv local $version
-	python ${mydir}/compile-file.py "$file"
+        pyenv local $version
+	    python ${mydir}/compile-file.py "$file"
     done
     short=$(basename $file .py)
     git add -f ${mydir}/bytecode-*/${short}*
--- xpython/byteop/byteop35pypy.py
+++ xpython/byteop/byteop35pypy.py
@@ -9,3 +9,6 @@ from xpython.byteop.byteoppypy import ByteOpPyPy
 class ByteOp35PyPy(ByteOp35, ByteOpPyPy):
     def __init__(self, vm):
         super(ByteOp35PyPy, self).__init__(vm)
+
+        # Fake up version information not already faked in super.
+        self.version = "3.5.3 (x-python, Oct 27 1955, 00:00:00)\n[PyPy with x-python]"
