77ad694dc7e868073b24ebe5c506ae8804005e8c
rocky <rocky@dusytfeet.com>
2021-09-15 22:49:14 -0400

    Improve fake version_info for PyPy 3.7

--- test/test-compat.sh
+++ test/test-compat.sh
@@ -4,14 +4,14 @@ set -e
 VERBOSE=${VERBOSE:-0}
 
 source ../admin-tools/pyenv-newest-versions
-
+PYVERSIONS="pypy3.7-7.3.5 pypy3.6-7.3.1 $PYVERSIONS"
 # FIXME: we need to fix up pypy bytecode. Skip for now
 for PYENV_VERSION in $PYVERSIONS; do
 	echo -----------------------------------------
 	echo Testing Python version $PYENV_VERSION ...
 	pyenv local $PYENV_VERSION
 
-	for bytecode_dir in bytecode-[2-3]* ; do
+	for bytecode_dir in bytecode-* ; do
 		echo testing $bytecode_dir ...
 		for file in ${bytecode_dir}/*.pyc ; do
 			(( $VERBOSE )) && echo $file
