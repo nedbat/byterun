9ffc1cd74435dd2dd4022f90a1f2ae1f1842baff
rocky <rocky@dusytfeet.com>
2021-11-07 04:52:49 -0500

    Administrivia

---
+++ admin-tools/setup-master.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PYTHON_VERSION=3.7.12
+
+# FIXME put some of the below in a common routine
+function finish {
+  cd $owd
+}
+
+export PATH=$HOME/.pyenv/bin/pyenv:$PATH
+owd=$(pwd)
+bs=${BASH_SOURCE[0]}
+if [[ $0 == $bs ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+mydir=$(dirname $bs)
+fulldir=$(readlink -f $mydir)
+cd $fulldir/..
+(cd ../python-xdis && git checkout master && pyenv local $PYTHON_VERSION) && git pull && \
+    git checkout master && pyenv local $PYTHON_VERSION && git pull
+cd $owd
+rm -v */.python-version >/dev/null 2>&1 || true
