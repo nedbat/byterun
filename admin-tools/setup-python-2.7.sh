684574de9d144431b79d68a5a4ef766af89c8cb0
rocky <rby@dusytfeet.com>
2021-11-10 15:36:22 -0500

    Administrivia

---
+++ admin-tools/setup-python-2.7.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PYTHON_VERSION=2.7.16
+pyenv local $PYTHON_VERSION
+
+owd=$(pwd)
+bs=${BASH_SOURCE[0]}
+if [[ $0 == $bs ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+mydir=$(dirname $bs)
+fulldir=$(readlink -f $mydir)
+cd $fulldir/..
+(cd ../python-xdis/admin-tools && . ./setup-python-2.4.sh && git pull)
+git checkout python-2.4-to-2.7
+cd $owd
+rm -v */.python-version >/dev/null 2>&1 || true
