b147bb243d493ad11666a0314f0bfd8786586cc0
rocky <rby@dusytfeet.com>
2021-11-10 15:17:02 -0500

    Administrivia

--- admin-tools/check-3.1-3.2-versions.sh
+++ admin-tools/check-3.1-3.2-versions.sh
@@ -10,13 +10,13 @@ trap finish EXIT
 
 cd $(dirname ${BASH_SOURCE[0]})
 
-if ! source ./pyenv-3.3-3.5-versions ; then
+if ! source ./pyenv-3.1-3.2-versions ; then
     exit $?
 fi
 cd ..
 [[ -f test/.python-version ]] && rm -v test/.python-version
-(cd ../python-xdis/admin-tools && source ./setup-python-3.3.sh)
-git checkout python-3.5
+(cd ../python-xdis/admin-tools && source ./setup-python-3.1.sh)
+git checkout python-3.1-to-3.2
 
 for version in $PYVERSIONS; do
     echo --- $version ---
