0faadefd0096e4c77963953a331de6a5cb6821c6
rocky <rby@dusytfeet.com>
2021-11-25 06:07:49 -0500

    Correct some PyPy 3.6 opcodes
    CALL_FUNCTION_KW and BUILD_MAP_UNPACK_WITH_CALL
    stick with older 3.5 semantics rather than the 3.6 change semantics

--- xpython/byteop/byteop36pypy.py
+++ xpython/byteop/byteop36pypy.py
@@ -1,5 +1,8 @@
 """Bytecode Interpreter operations for PyPy 3.6
 """
+
+from xpython.byteop.byteop24 import ByteOp24
+from xpython.byteop.byteop35 import ByteOp35
 from xpython.byteop.byteop36 import ByteOp36
 from xpython.byteop.byteoppypy import ByteOpPyPy
 
@@ -11,3 +14,9 @@ class ByteOp36PyPy(ByteOp36, ByteOpPyPy):
 
         # Fake up version information not already faked in super.
         self.version = "3.6.9 (x-python, Oct 27 1955, 00:00:00)\n[PyPy with x-python]"
+
+    # Python 3.6 changes the semantics of the below opcodes.
+    # However PyPy 3.6 uses the previous semantics, which we can pull
+    # from older Python versions
+    CALL_FUNCTION_KW = ByteOp24.CALL_FUNCTION_KW
+    BUILD_MAP_UNPACK_WITH_CALL = ByteOp35.BUILD_MAP_UNPACK_WITH_CALL
