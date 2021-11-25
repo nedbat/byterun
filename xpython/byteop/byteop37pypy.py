16c4da34521b63ca6b4f13c8b56bc526d32b0794
rocky <rby@dusytfeet.com>
2021-11-25 07:04:38 -0500

    Format CALL_METHOD{,_EX}

--- xpython/byteop/byteop37pypy.py
+++ xpython/byteop/byteop37pypy.py
@@ -9,9 +9,42 @@ from xpython.byteop.byteop37 import ByteOp37
 from xpython.byteop.byteoppypy import ByteOpPyPy
 
 
+def fmt_call_method(vm, argc: int, repr=repr) -> str:
+    """
+    formats function name (without enclosing object), and positional args.
+    """
+    pos_args = [vm.peek(i + 1) for i in range(argc)]
+
+    fn_name = vm.peek(argc + 2).__name__
+    return f""" {fn_name}({", ".join((repr(a) for a in pos_args))})"""
+
+
+def fmt_call_method_kw(vm, argc: int, repr=repr) -> str:
+    """
+    formats function name (without enclosing object), positional  and keyword args.
+    """
+    kwarg_names = vm.peek(1)
+    kwargs_count = len(kwarg_names)
+    kwargs_list = []
+    for i in range(kwargs_count):
+        kwargs_list.append(f"{kwarg_names[i]}={vm.peek(i+1)}")
+
+    pos_args = []
+    j = kwargs_count + 2
+    for i in range(argc - kwargs_count):
+        pos_args.append(vm.peek(i + j))
+
+    fn_name = vm.peek(argc + 3).__name__
+    return f""" {fn_name}({", ".join((repr(a) for a in pos_args + kwargs_list))})"""
+
+
 class ByteOp37PyPy(ByteOp37, ByteOpPyPy):
     def __init__(self, vm):
         super(ByteOp37PyPy, self).__init__(vm)
+
+        self.stack_fmt["CALL_METHOD"] = fmt_call_method
+        self.stack_fmt["CALL_METHOD_KW"] = fmt_call_method_kw
+
         self.is_pypy = True
         self.version = "3.7.12 (x-python, Oct 27 1955, 00:00:00)\n[PyPy with x-python]"
 
