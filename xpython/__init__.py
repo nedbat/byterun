d172a0064851444c3d36b63774084d07dfea963a
rocky <rby@dusytfeet.com>
2022-03-09 13:09:57 -0500

    __init.py__ lint

--- xpython/__init__.py
+++ xpython/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Copyright (C) 2020-2021 Rocky Bernstein
+"""Copyright (C) 2020-2022 Rocky Bernstein
 This program comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it
 under certain conditions.
@@ -8,8 +8,6 @@ See the documentation for the full license.
 """
 
 __docformat__ = "restructuredtext"
-
-from xpython.byteop import get_byteop
 
 from xpython.pyobj import (
     Function,
@@ -19,8 +17,20 @@ from xpython.pyobj import (
     traceback_from_frame,
     Generator,
 )
-from xpython.vm import PyVM, PyVMError, PyVMRuntimeError
 
+from xpython.version import __version__
+from xpython.vm import PyVM, PyVMError, PyVMRuntimeError
 from xpython.vmtrace import PyVMTraced, pretty_event_flags
 
-from xpython.version import __version__  # noqa
+__all__ = [
+    "Cell",
+    "Function",
+    "Generator",
+    "Method",
+    "PyVM",
+    "PyVMError",
+    "PyVMRuntimeError",
+    "PyVMTraced",
+    "Traceback",
+    "pretty_event_flags" "traceback_from_frame",
+]
