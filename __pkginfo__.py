8de1c61da5362e0fcd2c61260e40e417a6d4f401
rocky <rby@dusytfeet.com>
2023-01-26 02:21:23 -0500

    Administrivia

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -44,7 +44,7 @@ if (3, 0) <= PYTHON_VERSION_TRIPLE < (3, 3):
 else:
     click_version = ""
 
-install_requires = (["six", "xdis >= 6.0.3, < 6.1.0", "click%s" % click_version],)
+install_requires = (["six", "xdis >= 6.0.0, < 6.2.0", "click%s" % click_version],)
 
 py_modules = None
 short_desc = "Python cross-version byte-code interpeter"
