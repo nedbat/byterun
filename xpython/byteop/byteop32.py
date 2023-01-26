e6d8b7ebdda15528968375213478df88f9659d33
rocky <rby@dusytfeet.com>
2023-01-26 02:12:52 -0500

    Bang on 3.2 MAKE_CLOSURE -
    not there yet though

--- xpython/byteop/byteop32.py
+++ xpython/byteop/byteop32.py
@@ -46,56 +46,6 @@ class ByteOp32(ByteOp27):
     def convert_native_to_Function(self, frame, func):
         assert inspect.isfunction(func) or isinstance(func, Function)
 
-    # Changed from 2.7
-    # 3.2 has kwdefaults that aren't allowed in 2.7
-    def MAKE_FUNCTION(self, argc):
-        """
-        Creates a new function object, sets its __closure__ slot, and
-        pushes it on the stack. TOS is the code associated with the
-        function, TOS1 the tuple containing cells for the closure’s
-        free variables. The function also has argc default parameters,
-        which are found below the cells.
-        """
-        default_count, kw_default_count = parse_fn_counts_30_35(argc)
-
-        code = self.vm.pop()
-        name = code.co_name
-
-        if kw_default_count:
-            kw_default_pairs = self.vm.popn(2 * kw_default_count)
-            kwdefaults = dict(
-                kw_default_pairs[i : i + 2] for i in range(0, len(kw_default_pairs), 2)
-            )
-        else:
-            kwdefaults = {}
-
-        if default_count:
-            defaults = self.vm.popn(default_count)
-        else:
-            defaults = tuple()
-
-        # FIXME: DRY with code in byteop3{2,6}.py
-
-        globs = self.vm.frame.f_globals
-
-        fn = Function(
-            name=name,
-            code=code,
-            globs=globs,
-            argdefs=tuple(defaults),
-            closure=None,
-            vm=self.vm,
-            kwdefaults=kwdefaults,
-        )
-
-        self.vm.push(fn)
-
-    # Order of function here is the same as in:
-    # https://docs.python.org/3.2/library/dis.html#python-bytecode-instructions
-
-    # Note these are only the functions that aren't in the parent class
-    # here, Python 2.7
-
     def DUP_TOP_TWO(self):
         """Duplicates the reference on top of the stack."""
         a, b = self.vm.popn(2)
@@ -119,17 +69,16 @@ class ByteOp32(ByteOp27):
         """Pushes builtins.__build_class__() onto the stack. It is later called by CALL_FUNCTION to construct a class."""
         self.vm.push(__build_class__)
 
-    def MAKE_CLOSURE(self, argc):
+    def MAKE_CLOSURE(self, argc: int):
         """
         Creates a new function object, sets its ``__closure__`` slot, and
         pushes it on the stack. TOS is the code qualified name of the
-        function, TOS1 is the code associated with the function
-        and TOS2 is the tuple containing cells for the closure's free
-        variables. args is interpreted as in MAKE_FUNCTION;
-        the annotations and defauits are also in the same order below TOS2
+        function, TOS is the the code associated with the function and
+        TOS1 is the tuple containing cells for the closure's free
+        variables. The function asl has ``argc`` default parameters,
+        which are found below the cells.
         """
-        default_count, kw_default_count = parse_fn_counts_30_35(argc)
-        name = None
+        default_count, kw_default_count, annotate_count = parse_fn_counts_30_35(argc)
         closure, code = self.vm.popn(2)
 
         if kw_default_count:
@@ -145,7 +94,75 @@ class ByteOp32(ByteOp27):
         else:
             defaults = tuple()
 
+        if annotate_count:
+            annotate_names = self.vm.pop()
+            annotate_types = self.vm.popn(annotate_count)
+            n = len(annotate_names)
+            assert n == len(annotate_types)
+            annotations = {annotate_names[i]: annotate_types[i] for i in range(n)}
+        else:
+            annotations = {}
+
         # FIXME: DRY with code in MAKE_FUNCTION
+
+        globs = self.vm.frame.f_globals
+
+        fn = Function(
+            name=code.co_name,
+            code=code,
+            globs=globs,
+            argdefs=tuple(defaults),
+            closure=closure,
+            vm=self.vm,
+            kwdefaults=kwdefaults,
+            annotations=annotations,
+        )
+
+        self.vm.push(fn)
+
+
+    # Changed from 2.7
+    # 3.2 has kwdefaults that aren't allowed in 2.7
+    def MAKE_FUNCTION(self, argc: int):
+        """
+        Pushes a new function object on the stack. From bottom to top, the consumed stack must consist of:
+
+        * argc & 0xFF default argument objects in positional order
+        * (argc >> 8) & 0xFF pairs of name and default argument, with the name just below the object on the stack, for keyword-only parameters
+        * (argc >> 16) & 0x7FFF parameter annotation objects
+        * a tuple listing the parameter names for the annotations (only if there are ony annotation objects)
+        * the code associated with the function (at TOS1 if 3.3+ else at TOS for 3.0..3.2)
+        * the qualified name of the function (at TOS if 3.3+)
+        """
+        default_count, kw_default_count, annotate_count = parse_fn_counts_30_35(argc)
+
+        code = self.vm.pop()
+        name = code.co_name
+
+        if kw_default_count:
+            kw_default_pairs = self.vm.popn(2 * kw_default_count)
+            kwdefaults = dict(
+                kw_default_pairs[i : i + 2] for i in range(0, len(kw_default_pairs), 2)
+            )
+        else:
+            kwdefaults = {}
+
+        if default_count:
+            defaults = self.vm.popn(default_count)
+        else:
+            defaults = tuple()
+
+        if annotate_count:
+            annotate_names = self.vm.pop()
+            # annotate count includes +1 for the above names
+            annotate_objects = self.vm.popn(annotate_count - 1)
+            n = len(annotate_names)
+            assert n == len(annotate_objects)
+            annotations = {annotate_names[i]: annotate_objects[i] for i in range(n)}
+        else:
+            annotations = {}
+
+        # FIXME: DRY with code in byteop3{2,6}.py
 
         globs = self.vm.frame.f_globals
 
@@ -157,6 +174,7 @@ class ByteOp32(ByteOp27):
             closure=None,
             vm=self.vm,
             kwdefaults=kwdefaults,
+            annotations=annotations,
         )
 
         self.vm.push(fn)
@@ -218,7 +236,7 @@ class ByteOp32(ByteOp27):
         """Pops TOS from the stack and stores it as the current frames f_locals. This is used in class construction."""
         self.vm.frame.f_locals = self.vm.pop()
 
-    def RAISE_VARARGS(self, argc):
+    def RAISE_VARARGS(self, argc: int):
         """
         Raises an exception. argc indicates the number of arguments to the
         raise statement, ranging from 0 to 3. The handler will find
